fb5cc82f9e5a13a6b31ec992c21b90b93b97ccfd
puravi-238 <kajolsahu238@gmail.com>
2021-11-28 21:33:52 +0530

    added personal section

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
-<<<<<<< Updated upstream
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from "./components/signup.jsx";
 import Signin from "./components/signin.jsx";
 import Skills from "./components/skills.jsx";
-=======
-import Personal from "./components/Personal.jsx";
->>>>>>> Stashed changes
 import Experience from "./components/Experience";
+import Personal from "./components/Personal.jsx";
 import Education from './components/Education.jsx';
 import Footer from './components/Footer.jsx';
 
 function App() {
-<<<<<<< Updated upstream
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route exact path="/signin" element={<Signin />} />
-        <Route exact path="/skills" element={<Skills />} />
-        <Route exact path="/exp" element={<Experience />} />
-        <Route exact path="/edu" element={<Education />} />
-
-
-      </Routes>
-    </Router >
-  );
-=======
     return ( <
-        div className = "App" >
+        Router >
         <
         Navbar / >
         <
-        Personal / >
+        Routes >
         <
-        Experience / >
-        <
-        Education / >
-        <
-        /div>
+        Route path = "/signup"
+        element = { < Signup / > }
+        /> <
+        Route exact path = "/signin"
+        element = { < Signin / > }
+        /> <
+        Route exact path = "/skills"
+        element = { < Skills / > }
+        /> <
+        Route exact path = "/exp"
+        element = { < Experience / > }
+        /> <
+        Route exact path = "/edu"
+        element = { < Education / > }
+        /> <
+        Route exact path = "/per"
+        element = { < Personal / > }
+        /> <
+        /Routes> <
+        /Router >
     );
->>>>>>> Stashed changes
 }
 
 export default App;
