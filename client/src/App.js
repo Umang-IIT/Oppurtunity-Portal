500b5b78c0a7096d0b7100ff4ef6daa6df8547ac
anjaniit23 <anjaniit23@gmail.com>
2021-11-29 15:21:07 +0530

    added server

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,10 @@ import Skills from "./components/skills.jsx";
 import Experience from "./components/Experience";
 import Personal from "./components/Personal.jsx";
 import Education from "./components/Education.jsx";
-import Footer from "./components/Footer.jsx";
+// import Footer from "./components/Footer.jsx";
 import Home from "./components/Home.jsx";
 
 function App() {
-<<<<<<< HEAD
-    return (
-        <Router >
-            <Navbar />
-            <Routes >
-                <Route path="/signup" element={< Signup />} />
-                < Route exact path="/signin" element={< Signin />} />
-                < Route exact path="/skills" element={< Skills />} />
-                <Route exact path="/exp" element={< Experience />} />
-                <Route exact path="/edu" element={< Education />} />
-                <Route exact path="/per" element={< Personal />} />
-
-            </Routes>
-            {/* <Footer /> */}
-        </Router >
-    );
-=======
   return (
     <Router>
       <Navbar />
@@ -42,7 +25,6 @@ function App() {
       </Routes>{" "}
     </Router>
   );
->>>>>>> 45b8bc8dbb77327e82d54f26203d507693c759ac
 }
 
 export default App;
